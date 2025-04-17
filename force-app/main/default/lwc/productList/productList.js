8264b2ae6812f911bb25fb693ad84e55872222a2
jlcms <santosjlcm@gmail.com>
2025-04-17 23:55:30 +0100

    first commit

---
+++ force-app/main/default/lwc/productList/productList.js
@@ -0,0 +1,32 @@
+import { LightningElement, track, wire } from 'lwc';
+import getStoredProducts from '@salesforce/apex/ProductService.getStoredProducts';
+import fetchAndStoreProducts from '@salesforce/apex/ProductService.fetchAndStoreProducts';
+
+export default class ProductList extends LightningElement {
+    @track products;
+    @track error;
+
+    @wire(getStoredProducts)
+    wiredProducts({ error, data }) {
+        if (data) {
+            this.products = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error.body.message;
+            this.products = undefined;
+        }
+    }
+
+    handleFetch() {
+        fetchAndStoreProducts()
+            .then(() => {
+                return getStoredProducts()
+                    .then(result => {
+                        this.products = result;
+                    });
+            })
+            .catch(error => {
+                this.error = error.body.message;
+            });
+    }
+}
